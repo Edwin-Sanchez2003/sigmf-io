8d44e35bff9556f746ac061e1dd785d45ac54dba
Edwin Sanchez <edwinsanchez2003@gmail.com>
2026-05-17 18:40:17 -0400

    added notes on SigMF TODO list; tested SigMFDataset.getSamples() function.

--- main.cpp
+++ main.cpp
@@ -5,10 +5,19 @@
 
 int main()
 {
+    /*
+     * TODO:
+     * * Update SigMFDataset class to handle channels.
+     * * Implement SigMFRecording class.
+     * * Setup SigMFRecording class to be the main public interface of which to interact with a SigMF Dataset & Metadata.
+     *  -> A SigMFDataset doesn't know of it's own header bytes; you need metadata to do that (in fact, you need the meta
+     *    data in order to interact with most of a SigMFDataset.
+     */
+
     SigMFDataset sigmfData = SigMFDataset("/var/home/edwsanch/Downloads/trimmedSamples.sigmf-data", SigMFDataType("cf32_le"), 0, 1, 0);
 
     std::cout << sigmfData.getDataType().getRawDataType() << '\n';
-    for(const auto& sample: sigmfData.getSamples(0, 10))
+    for(const auto& sample: sigmfData.getSamples(0, 100))
         std::cout << sample.real() << " + " << sample.imag() << "i\n";
     return 0;
 }
