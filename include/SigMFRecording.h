610d64aa80ea8a0270cb45a5720e2ddc6d421810
Edwin Sanchez <edwinsanchez2003@gmail.com>
2026-08-01 19:34:16 -0400

    initial construction of SigMFRecording class... I hate it...

--- include/SigMFRecording.h
+++ include/SigMFRecording.h
@@ -1,15 +1,30 @@
 #ifndef SIGMFRECORDING_H
 #define SIGMFRECORDING_H
 
+#include "SigMFDataset.h"
+#include <optional>
+#include <string>
+#include <jsoncons/json.hpp>
+
 class SigMFRecording
 {
 public:
+    std::optional<SigMFDataset> dataset;
+
+public:
+    explicit SigMFRecording();
     SigMFRecording(std::string file_path);
 
-    std::string file_path() const { return this->file_path_; }
+    std::string meta_path() const { return this->meta_path_; }
+    std::string data_path() const { return this->dataset.value().getDatasetPath(); }
 
 private:
-    std::string file_path_;
+    static constexpr std::string META_EXT = ".sigmf-meta";
+    static constexpr std::string DATA_EXT = ".sigmf-data";
+    std::string meta_path_;
+    jsoncons::json meta_;
+
+    bool endsWith(const std::string& value, const std::string& ending) const;
 };
 
 #endif // SIGMFRECORDING_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+#include "SigMFRecording.h"
 #include "SigMFDataType.h"
 #include "SigMFDataset.h"
 #include "sigmf.h"
@@ -8,6 +9,7 @@ using SigMFCapture = sigmf::Capture<sigmf::core::DescrT>;
 
 int main()
 {
+    // Simple Initialization of SigMFDataset object.
     SigMFDataset sigmfData = SigMFDataset("/var/home/edwsanch/Downloads/trimmedSamples.sigmf-data", SigMFDataType("cf32_le"), 1, 0);
     std::cout << sigmfData.getDataType().getRawDataType() << '\n';
     std::vector<SigMFCapture> captures;
@@ -19,6 +21,15 @@ int main()
     for(const std::complex<double>& sample: samples)
         std::cout << sample.real() << " + " << sample.imag() << "i\n";
 
+    // Initial Initialization of SigMFRecording object.
+    SigMFRecording recording = SigMFRecording("/var/home/edwsanch/Downloads/trimmedSamples.sigmf-data");
+    SigMFRecording();
+    auto samples2 = recording.dataset.value().getSamples<std::complex<double>>(captures, 0, 10);
+    std::cout << "Sample count returned: " << samples2.size() << '\n';
+
+    for(const std::complex<double>& sample: samples2)
+        std::cout << sample.real() << " + " << sample.imag() << "i\n";
+
     return 0;
 }
 
@@ -26,5 +37,15 @@ int main()
 // Figuring out how I want the API to feel.
 void test()
 {
+    // Goal: Load SigMF Recording.
+    //SigMFRecording recording = SigMFRecording("/path/to/file.sigmf-meta");
+
+    // vector/some sort of iterable array-like structure of the first 100 samples (0-99) of the recording.
+    // due to SigMF wonkiness, this is specifically the 1st channel.
+    //recording[0:100]; // would need to implement array slicing as operator overloading...
+
+
+    //recording.global
+
 
 }
