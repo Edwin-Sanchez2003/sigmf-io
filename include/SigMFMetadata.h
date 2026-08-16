3d87e634d0d6cc24940db4f2e72e67afa9d241ae
Edwin Sanchez <edwinsanchez2003@gmail.com>
2026-08-16 19:13:22 -0400

    starting changes to Metadata & recording objects

--- include/SigMFMetadata.h
+++ include/SigMFMetadata.h
@@ -7,40 +7,37 @@
 #include <utility>
 #include <jsoncons/json.hpp>
 #include <jsoncons_ext/jsonpointer/jsonpointer.hpp>
+#include "sigmf_global.h"
+#include "sigmf_capture.h"
+#include "sigmf_annotation.h"
 
 class SigMFMetadata
 {
 public:
+    SigMFGlobal global;
+    std::vector<SigMFCapture> captures;
+    std::vector<SigMFAnnotation> annotations;
+public:
     SigMFMetadata();
+    SigMFMetadata(
+        const SigMFGlobal& global = SigMFGlobal(),
+        const std::vector<SigMFCapture>& captures = {},
+        const std::vector<SigMFAnnotation>& annotations = {}
+    );
     explicit SigMFMetadata(const std::string& meta_path);
     explicit SigMFMetadata(const jsoncons::json& meta);
 
     std::string meta_path() const { return this->meta_path_; }
 
-    template <typename T>
-    T get(const std::string& json_pointer) const;
-
-    template <typename T>
-    void set(const std::string& json_pointer, T&& value);
-
     void save(const std::string& file_path, bool overwrite = false);
+
+    // TODO: Need to write useful/helper functions for common actions using metadata?
 
 private:
     static inline constexpr std::string META_EXT = ".sigmf-meta";
     std::string meta_path_;
-    jsoncons::json meta_;
 
     bool ends_with(const std::string& value, const std::string& ending) const;
 };
-
-template <typename T>
-T SigMFMetadata::get(const std::string& json_pointer) const {
-    return jsoncons::jsonpointer::get(this->meta_, json_pointer).template as<T>();
-}
-
-template <typename T>
-void SigMFMetadata::set(const std::string& json_pointer, T&& value) {
-    jsoncons::jsonpointer::get(this->meta_, json_pointer, true) = std::forward<T>(value);
-}
 
 #endif // SIGMFMETADATA_H
--- include/SigMFRecording.h
+++ include/SigMFRecording.h
@@ -2,6 +2,7 @@
 #define SIGMFRECORDING_H
 
 #include "SigMFDataset.h"
+#include "SigMFMetadata.h"
 #include <optional>
 #include <string>
 #include <jsoncons/json.hpp>
@@ -9,22 +10,25 @@
 class SigMFRecording
 {
 public:
-    std::optional<SigMFDataset> dataset;
-
+    SigMFDataset data;
+    SigMFMetadata meta;
 public:
-    explicit SigMFRecording();
-    SigMFRecording(std::string file_path);
+    SigMFRecording();
+    explicit SigMFRecording(const std::string& file_path);
 
     std::string meta_path() const { return this->meta_path_; }
     std::string data_path() const { return this->dataset.value().getDatasetPath(); }
 
+    /* TODO: helper functions for accessing data from a recording using the metadata.
+     * - get samples given indices
+     * - get samples given captures / capture indices
+     * - get samples given annotation/annotation indices
+     * - iterators over certain types, etc.
+    */
+
 private:
     static constexpr std::string META_EXT = ".sigmf-meta";
     static constexpr std::string DATA_EXT = ".sigmf-data";
-    std::string meta_path_;
-    jsoncons::json meta_;
-
-    bool ends_with(const std::string& value, const std::string& ending) const;
 };
 
 #endif // SIGMFRECORDING_H
